016f909a89d0e754d243c71975578af509e45f7b
bilalahmad99 <82260050+bilalahmad72@users.noreply.github.com>
2024-12-11 22:45:05 +0500

    foundations done

---
+++ JSFoundations/hello.js
@@ -0,0 +1,29 @@
+// in now a days semi colon at the end of line is optional for JAVASCRIPT
+console.log("Hello World");
+
+// to run the file we use => node filename with extension
+// as right now file name is ( hello.js ), so we run node hello.js
+// after run the command will see the output of the file
+
+// shortcut command for console.log is log on VS Code
+
+// It is necessary to save the file before run command => ( node hello.js)
+
+console.log("Hello World!");
+
+console.log("Pakistan Zindabad!");
+
+console.log("How are you!");
+
+console.log("This is Javascript!");
+console.log("loading...");
+
+// the see the structure how javascript code is run? which compiler is used? what compiler do?  see the images ( first.png & second.png)
+
+// JIT compiler is used ( Just in Time )
+
+// There are some other software exists that can run the javascript or used for runtime of javascript ( node, bun, JavaScript Core, Deno etc )...
+
+// search these software on the google to learn about these
+
+// but mostly node is used, so that's why node will be used
