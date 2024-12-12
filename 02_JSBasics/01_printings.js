1047d54254bddb64f70d0486d51f40463dcf343d
bilalahmad99 <82260050+bilalahmad72@users.noreply.github.com>
2024-12-12 16:54:02 +0500

    understand datatypes in js

---
+++ 02_JSBasics/01_printings.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+console.table({ name: "Bilal", label: "My Table" });
+
+process.stdout.write("Bilal ");
+process.stdout.write("Ahmad\n");
+
+console.log("New");
+
+console.warn("This is warning");
+
+console.info("This is info");
