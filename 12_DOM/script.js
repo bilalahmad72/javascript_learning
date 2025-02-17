887e8a6fb821357885233a40cdb7cc84e21f787a
Bilal Ahmad <bilalahmad72.official@gmail.com>
2025-02-18 01:51:36 +0500

    learning

--- 12_DOM/script.js
+++ 12_DOM/script.js
@@ -1,4 +1,4 @@
-console.log("Hello I am Bilal Ahmad");
+console.log('Hello I am Bilal Ahmad');
 
 // these are the examples of window object under BOM , because we are taking height and width of the screen, navigation, location
 console.log(window.outerHeight);
@@ -13,8 +13,7 @@ console.log(location.port);
 console.log(screen.availHeight);
 console.log(screen.colorDepth);
 
-
-// search more about of the mdn about navigator 
+// search more about of the mdn about navigator
 
 /// to get access of the whole document we use document keyword
 
@@ -22,10 +21,5 @@ console.log(document);
 
 // now we get any atttibute inside the document in different ways, like id, tag name, class name etc...
 
-console.log(document.getElementsByTagName("h1"));
-console.log(document.getElementsByTagName("p"));
-
-
-
-
-
+console.log(document.getElementsByTagName('h1'));
+console.log(document.getElementsByTagName('p'));
