519b9c47e7b69c5700c9cef9054009bf57b236ad
Bilal Ahmad <bilalahmad72.official@gmail.com>
2025-02-18 00:53:22 +0500

    DOM Learning

---
+++ 12_DOM/script.js
@@ -0,0 +1,31 @@
+console.log("Hello I am Bilal Ahmad");
+
+// these are the examples of window object under BOM , because we are taking height and width of the screen, navigation, location
+console.log(window.outerHeight);
+console.log(window.outerWidth);
+console.log(window.navigator.cookieEnabled);
+console.log(window.devicePixelRatio);
+console.log(location.host);
+console.log(location.hostname);
+console.log(location.href);
+console.log(location.pathname);
+console.log(location.port);
+console.log(screen.availHeight);
+console.log(screen.colorDepth);
+
+
+// search more about of the mdn about navigator 
+
+/// to get access of the whole document we use document keyword
+
+console.log(document);
+
+// now we get any atttibute inside the document in different ways, like id, tag name, class name etc...
+
+console.log(document.getElementsByTagName("h1"));
+console.log(document.getElementsByTagName("p"));
+
+
+
+
+
